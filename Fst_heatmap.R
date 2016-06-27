7ee50d4616864d95fead79e35d58e0b4214f82bd
Francisco Pina Martins <f.pinamartins@gmail.com>
2016-06-27 14:14:32 +0100

    Less hardcoding. Or at least easier to understand.

--- Fst_heatmap.R
+++ Fst_heatmap.R
@@ -14,23 +14,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Fst_heatmap.R. If not, see <http://www.gnu.org/licenses/>.
 
+# Set some variables
+double_tri_matrix = "/home/francisco/Dropbox/Science/PhD/Sequenom_SNPs/3rd Generation/Fst/FST_Tri_Matix_NN-lower_N-upper.csv"
+neutral_fst = "/home/francisco/Dropbox/Science/PhD/Sequenom_SNPs/3rd Generation/Fst/Neutrals.txt"
+non_nutral_fst = "/home/francisco/Dropbox/Science/PhD/Sequenom_SNPs/3rd Generation/Fst/Neutrals.txt"
 
 # This will draw the color matrix plot
 library(corrplot)
-trimatirxdata <- read.csv("~/Dropbox/Science/PhD/Sequenom_SNPs/GenePop/With PIS/Fst_matrix_lowerNn_upperN_noPIS_names.txt", sep="\t", header=TRUE)
+trimatirxdata <- read.csv(double_tri_matrix, sep="\t", header=TRUE, row.names=1)
 matrixdata <- as.matrix(trimatirxdata)
 corrplot(matrixdata,method="shade", is.corr = FALSE)
 
 # This will draw the line plots
 
-Neut_line_data <- read.csv("~/Dropbox/Science/PhD/Sequenom_SNPs/GenePop/With PIS/Neutrals/Fst_values.txt", sep="\t", header=FALSE)
+Neut_line_data <- read.csv(neutral_fst, sep="\t", header=FALSE)
 Neut_matrix <- as.matrix(Neut_line_data)
 Neut_matrix[Neut_matrix<0] <- 0
 
 dens <- density(Neut_matrix)
 plot(dens$x,length(Neut_matrix)*dens$y,type="l",xlab="Value",ylab="Density", col="black")
 
-Non_neut_line_data <- read.csv("~/Dropbox/Science/PhD/Sequenom_SNPs/GenePop/With PIS/Non-neutral/Fst_values.txt", sep="\t", header=FALSE)
+Non_neut_line_data <- read.csv(non_nutral_fst, sep="\t", header=FALSE)
 Non_neut_matrix <- as.matrix(Non_neut_line_data)
 Non_neut_matrix[Non_neut_matrix<0] <- 0
 
